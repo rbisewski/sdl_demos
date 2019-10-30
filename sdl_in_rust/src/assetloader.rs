60ddc61203f1ed35327f7553ba702fca4dacbbd4
Robert Bisewski <contact@ibiscybernetics.com>
2019-10-30 15:59:30 -0500

    work on functionalizing the Texture init logic

--- sdl_in_rust/src/assetloader.rs
+++ sdl_in_rust/src/assetloader.rs
@@ -4,7 +4,6 @@
 extern crate sdl2;
 
 use std::path::PathBuf;
-use sdl2::render::*;
 use sdl2::surface::*;
 
 pub const UI_GFX_LOCATION: &str = "img/ui/";
@@ -18,51 +17,46 @@ pub fn init_textures() -> Result<(), String> {
         Ok(path_to_mouse_gfx_canon) => path_to_mouse_gfx_canon,
     };
 
-    let sdl_tmp_surface = Surface::load_bmp(path_to_mouse_gfx_canon);
+    let tmp_surface = match Surface::load_bmp(path_to_mouse_gfx_canon) {
+        Err(e) => return Err(e.to_string()),
+        Ok(tmp_surface) => tmp_surface,
+    };
 
-    //let MOUSE_GFX = Texture::create_texture_from_surface(sdl_tmp_surface);
+    let tmp_surface_canvas = match tmp_surface.into_canvas() {
+        Err(e) => return Err(e.to_string()),
+        Ok(tmp_surface_canvas) => tmp_surface_canvas,
+    };
 
-    //// load the mouse graphics into memory
-    //tmp_surface = SDL_LoadBMP((char*) img_path);
-    //memset(&img_path, 0, 256*sizeof(char));
+    let tmp_texture_creator = tmp_surface_canvas.texture_creator();
+
+    let path_to_mouse_gfx_canon_copy = match path_to_mouse_gfx.canonicalize() {
+        Err(e) => return Err(e.to_string()),
+        Ok(path_to_mouse_gfx_canon_copy) => path_to_mouse_gfx_canon_copy,
+    };
+
+    let tmp_surface_copy = match Surface::load_bmp(path_to_mouse_gfx_canon_copy) {
+        Err(e) => return Err(e.to_string()),
+        Ok(tmp_surface_copy) => tmp_surface_copy,
+    };
+
+    // TODO: fix this
+    let _mouse_gfx = tmp_texture_creator.create_texture_from_surface(tmp_surface_copy);
+
     //MOUSE_GFX = SDL_CreateTextureFromSurface(RENDERER, tmp_surface);
     //SDL_FreeSurface(tmp_surface);
     //tmp_surface = NULL;
-
-    //// ensure calloc worked
-    //if (!MOUSE_GFX) {
-    //    return "InitTextures() --> unable to calloc for mouse GFX";
-    //}
 
     //// attempt to calloc mem for the desert tile textures
     //DESERT_TILES = (SDL_Texture**) calloc(LEVEL_TYPE_DESERT_MAX,
     //  sizeof(SDL_Texture*));
 
-    //// ensure calloc worked
-    //if (!DESERT_TILES) {
-    //    DeleteTextures();
-    //    return "InitTextures() --> unable to calloc for desert tiles";
-    //}
-
     //// attempt to calloc mem for the grass tile textures
     //GRASS_TILES = (SDL_Texture**) calloc(LEVEL_TYPE_GRASS_MAX,
     //  sizeof(SDL_Texture*));
 
-    //// ensure calloc worked
-    //if (!GRASS_TILES) {
-    //    DeleteTextures();
-    //    return "InitTextures() --> unable to calloc for grass tiles";
-    //}
-
     //// attempt to calloc mem for the swamp tile textures
     //SWAMP_TILES = (SDL_Texture**) calloc(LEVEL_TYPE_SWAMP_MAX,
     //  sizeof(SDL_Texture*));
-
-    //// ensure calloc worked
-    //if (!SWAMP_TILES) {
-    //    DeleteTextures();
-    //    return "InitTextures() --> unable to calloc for swamp tiles";
-    //}
 
     //// Load all of the desert tiles.
     //for (i = 1; i < LEVEL_TYPE_DESERT_MAX; i++) {
@@ -137,4 +131,40 @@ pub fn init_textures() -> Result<(), String> {
     //}
 
     Ok(())
+}
+
+pub fn bmp_to_texture(path: PathBuf) -> Result<sdl2::render::Texture<'static>, String> {
+
+    let path_to_mouse_gfx_canon = match path.canonicalize() {
+        Err(e) => return Err(e.to_string()),
+        Ok(path_to_mouse_gfx_canon) => path_to_mouse_gfx_canon,
+    };
+
+    let tmp_surface = match Surface::load_bmp(path_to_mouse_gfx_canon) {
+        Err(e) => return Err(e.to_string()),
+        Ok(tmp_surface) => tmp_surface,
+    };
+
+    let tmp_surface_canvas = match tmp_surface.into_canvas() {
+        Err(e) => return Err(e.to_string()),
+        Ok(tmp_surface_canvas) => tmp_surface_canvas,
+    };
+
+    let path_to_mouse_gfx_canon_copy = match path.canonicalize() {
+        Err(e) => return Err(e.to_string()),
+        Ok(path_to_mouse_gfx_canon_copy) => path_to_mouse_gfx_canon_copy,
+    };
+
+    let tmp_surface_copy = match Surface::load_bmp(path_to_mouse_gfx_canon_copy) {
+        Err(e) => return Err(e.to_string()),
+        Ok(tmp_surface_copy) => tmp_surface_copy,
+    };
+
+    let tmp_texture_creator = tmp_surface_canvas.texture_creator();
+
+    // TODO: get this to use a non-borrowed value
+    match tmp_texture_creator.create_texture_from_surface(tmp_surface_copy) {
+        Err(e) => return Err(e.to_string()),
+        Ok(initialized_texture) => return Ok(initialized_texture),
+    };
 }
